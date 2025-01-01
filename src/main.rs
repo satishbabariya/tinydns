21481827ec90657ce4668b9453703bc531692240
Satish Babariya <satish.babariya@gmail.com>
2025-01-01 20:58:12 +0530

    Enhance DNS server logging and error handling for query processing

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 mod dns;
 
 use log::LevelFilter;
+use log::{error, info, warn};
 use std::error::Error;
 use tokio::net::UdpSocket;
 use tokio::signal::unix::{signal, SignalKind};
@@ -10,7 +11,7 @@ use crate::dns::message::DNSMessage;
 
 async fn run_dns_server(addr: &str) -> Result<(), Box<dyn Error>> {
     let socket = UdpSocket::bind(addr).await?;
-    println!("DNS Server is running on {}", addr);
+    info!("DNS Server is running on {}", addr);
 
     // Reuse the same socket for forwarding requests
     let remote_dns_server = "8.8.8.8:53"; // Google's public DNS server
@@ -21,21 +22,30 @@ async fn run_dns_server(addr: &str) -> Result<(), Box<dyn Error>> {
     loop {
         tokio::select! {
             _ = shutdown_signal.recv() => {
-                println!("Shutdown signal received. Closing server...");
+                info!("Shutdown signal received. Closing server...");
                 break;
             }
             result = socket.recv_from(&mut buf) => {
                 match result {
                     Ok((len, addr)) => {
-                        let message = DNSMessage::parse(&buf[0..len]);
-                        println!("Received DNS Message: {:?}", message);
+                        // match DNSMessage::parse(&buf[0..len]) {
+                        //     Ok(message) => info!("Received DNS Message from {}: {:?}", addr, message),
+                        //     Err(e) => warn!("Failed to parse DNS message from {}: {}", addr, e),
+                        // }
 
-                        let response = forward_query(&socket, remote_dns_server, &buf[0..len]).await?;
-                        socket.send_to(&response, addr).await?;
+                        let message = DNSMessage::parse(&buf[0..len]);
+                        info!("Received DNS Message from {}: {:?}", addr, message);
+
+                        match forward_query(&socket, remote_dns_server, &buf[0..len]).await {
+                            Ok(response) => {
+                                if let Err(e) = socket.send_to(&response, addr).await {
+                                    error!("Error sending response to {}: {}", addr, e);
+                                }
+                            }
+                            Err(e) => error!("Error forwarding query to remote server: {}", e),
+                        }
                     }
-                    Err(e) => {
-                        eprintln!("Error receiving from socket: {}", e);
-                    }
+                    Err(e) => error!("Error receiving from socket: {}", e),
                 }
             }
         }
@@ -53,7 +63,12 @@ async fn main() -> Result<(), Box<dyn Error>> {
 
     let port = std::env::var("PORT").unwrap_or_else(|_| "53".to_string());
     let addr = format!("0.0.0.0:{}", port);
-    run_dns_server(&addr).await
+
+    info!("tinydns v0.1.0");
+    if let Err(e) = run_dns_server(&addr).await {
+        error!("DNS server encountered an error: {}", e);
+    }
+    Ok(())
 }
 
 // Forward the query to another DNS server (e.g., 8.8.8.8)
@@ -62,19 +77,26 @@ async fn forward_query(
     remote_dns_server: &str,
     query: &[u8],
 ) -> Result<Vec<u8>, Box<dyn Error>> {
-    // Set timeout duration (e.g., 1 seconds)
     let timeout_duration = Duration::from_secs(5);
 
     // Send the query to the remote DNS server
-    socket.send_to(query, remote_dns_server).await?;
+    if let Err(e) = socket.send_to(query, remote_dns_server).await {
+        error!("Error sending query to remote DNS server: {}", e);
+        return Err(Box::new(e));
+    }
 
-    // Set up timeout for receiving the response
     let mut response = vec![0u8; 512]; // Standard DNS response buffer
     let res = timeout(timeout_duration, socket.recv_from(&mut response)).await;
 
     match res {
-        Ok(Ok(_)) => Ok(response),      // Successfully received the response
-        Ok(Err(e)) => Err(Box::new(e)), // Error occurred while receiving
-        Err(_) => Err("Timeout while waiting for response".into()), // Handle timeout
+        Ok(Ok(_)) => Ok(response), // Successfully received the response
+        Ok(Err(e)) => {
+            error!("Error receiving response from remote server: {}", e);
+            Err(Box::new(e))
+        }
+        Err(_) => {
+            warn!("Timeout while waiting for response from remote DNS server");
+            Err("Timeout while waiting for response".into())
+        }
     }
 }
